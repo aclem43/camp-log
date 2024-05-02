0bc8fb4952341f7f6bf94ad0100bb8a03ce1ba92
aclem43 <aclemeur@gmail.com>
2024-05-02 21:23:51 +1000

    Reworked locations page

--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { remult } from 'remult'
+import { mdiDelete, mdiEye } from '@mdi/js'
 import { Location } from '@/shared/models/Location'
 
 const locations = ref<Location[]>([])
 
-onMounted(async () => {
+async function loadLocations() {
   locations.value = await remult.repo(Location).find({})
+}
+
+onMounted(async () => {
+  await loadLocations()
 })
+
+async function deleteLocation(location: Location) {
+  // eslint-disable-next-line no-alert
+  const confirm = window.confirm('Are you sure you want to delete this location?')
+  if (!confirm)
+    return
+  await remult.repo(Location).delete(location)
+  await loadLocations()
+}
 </script>
 
 <template>
@@ -25,14 +39,26 @@ onMounted(async () => {
             >
               Add Location
             </v-btn>
-            <v-list>
-              <v-list-item
-                v-for="loc in locations"
-                :key="loc.id"
-                :title="loc.name"
-                :subtitle="loc.address"
-              />
-            </v-list>
+            <v-data-table
+              density="compact"
+              :headers="[
+                { title: 'Name', value: 'name', sortable: true },
+                { title: 'Notes', value: 'notes', sortable: true },
+                { title: 'Address', value: 'address', sortable: true },
+                { title: 'Actions', value: 'actions', sortable: false, width: '100px' },
+              ]"
+              :items="locations"
+              :items-per-page="5"
+              :sort-by="[{ key: 'dateStart', order: 'desc' }]"
+            >
+              <template #[`item.actions`]="{ item }">
+                <div class="d-flex ga-2 align-center justify-center">
+                  <v-btn density="compact" color="error" @click="deleteLocation(item)">
+                    <v-icon :icon="mdiDelete" />
+                  </v-btn>
+                </div>
+              </template>
+            </v-data-table>
           </v-card-text>
         </v-card>
       </v-col>
