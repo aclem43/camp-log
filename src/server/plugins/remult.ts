ab138dd1863c5b3e8be59f5ae267bb2a69ab8ba7
Alexi <59304717+aclem43@users.noreply.github.com>
2025-02-20 00:37:08 +0000

    Added json DB option

--- src/server/plugins/remult.ts
+++ src/server/plugins/remult.ts
@@ -1,13 +1,24 @@
 import { remultExpress } from 'remult/remult-express'
 import { PostgresDataProvider } from 'remult/postgres'
-import { SqlDatabase, type UserInfo, repo } from 'remult'
+import { JsonDataProvider, SqlDatabase, type UserInfo, repo } from 'remult'
 import type { Request } from 'express'
-import { isDev } from '../config'
+import { JsonEntityFileStorage } from 'remult/server'
+import { isDev, proc } from '../config'
 import { pool } from '../db/pool'
 import { Activity } from '../../shared/models/Activity'
 import { Location } from '../../shared/models/Location'
 import { ActivityTemplate } from '../../shared/models/ActivityTemplate'
 import { Log } from '../../shared/models/Log'
+
+let dataProvider
+if (proc.env.DATABASE_TYPE === "JSON") {
+  dataProvider = new JsonDataProvider(new JsonEntityFileStorage("./db"))
+}
+else {
+  dataProvider = new SqlDatabase(new PostgresDataProvider(pool, {
+    orderByNullsFirst: false,
+  }))
+}
 
 export const api = remultExpress(
   {
@@ -19,8 +30,6 @@ export const api = remultExpress(
       Log,
 
     ],
-    dataProvider: new SqlDatabase(new PostgresDataProvider(pool, {
-      orderByNullsFirst: false,
-    })),
+    dataProvider,
   },
 )
